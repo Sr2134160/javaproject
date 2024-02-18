17d28bc218f674ca0866ec7864055794612bb722
Sr2134160 <Sr2134160@gmail.com>
2024-02-18 15:36:43 +0530

    demo'

---
+++ Demo.java
@@ -0,0 +1,15 @@
+package com.java.exp;
+
+public class Demo {
+	public void test() {
+		int i=10; int j=15;
+		System.out.println(i);
+		System.out.println(j);
+		}
+
+	public static void main(String[] args) {
+		Demo d=new Demo();
+		d.test();
+	}
+
+}
